b48811c045968b51e463c5a5fc0398c769b232d5
llrboy <139751767+llrboy@users.noreply.github.com>
2025-10-26 20:40:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,16 +9,9 @@
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
-#rm -rf feeds/luci/applications/luci-app-mosdns
-#rm -rf feeds/packages/net/mosdns
-#rm -rf feeds/packages/net/v2ray-geodata
-
-# rm -rf feeds/luci/themes/luci-theme-argon
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
-
-#git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 
 # 添加 onliner 插件
 git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app-onliner
@@ -29,4 +22,3 @@ git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-ap
 # 添加 opentomcat 主题
 git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
 
-# git clone 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
