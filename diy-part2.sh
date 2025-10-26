5a408b135f9a7ae5f35f15920520c00f9162ef38
llrboy <139751767+llrboy@users.noreply.github.com>
2025-10-26 20:10:17 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,10 @@ date_version=$(date +"%y.%m.%d")
 orig_version=$(cat "package/lean/default-settings/files/zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
 sed -i "s/${orig_version}/R${date_version} by LERAN/g" package/lean/default-settings/files/zzz-default-settings
 
+#替换为 JerryKuKu 的 Argon
+rm openwrt/package/lean/luci-theme-argon -rf
+git clone 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+
 # 修改默认主题
 sed -i 's/luci-theme-bootstrap/luci-theme-design/' feeds/luci/collections/luci/Makefile
 
