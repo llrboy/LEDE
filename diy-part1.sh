c60b76bcefdba07fe6c0a8a9642fadb3b0bc3161
llrboy <139751767+llrboy@users.noreply.github.com>
2025-11-06 11:21:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,6 +19,8 @@ git clone https://github.com/nhhqgirl/luci-app-onliner.git package/lean/luci-app
 # 添加 PowerOff 插件
 git clone https://github.com/nhhqgirl/luci-app-poweroff.git package/lean/luci-app-poweroff
 
-# 添加 opentomcat 主题
+# 添加主题
 git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
+
+git clone --depth=1 -b master https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
